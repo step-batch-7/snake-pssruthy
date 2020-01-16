9434d020707fada8c08f27908da1024c7be4d79d
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-01-16 23:27:57 +0530

    Updated Direction class identifiers as private

--- direction.js
+++ direction.js
@@ -1,19 +1,21 @@
 class Direction {
+  #heading;
+  #deltas;
   constructor(initialHeading) {
-    this.heading = initialHeading;
-    this.deltas = {};
-    this.deltas[EAST] = [1, 0];
-    this.deltas[WEST] = [-1, 0];
-    this.deltas[NORTH] = [0, -1];
-    this.deltas[SOUTH] = [0, 1];
+    this.#heading = initialHeading;
+    this.#deltas = {};
+    this.#deltas[EAST] = [1, 0];
+    this.#deltas[WEST] = [-1, 0];
+    this.#deltas[NORTH] = [0, -1];
+    this.#deltas[SOUTH] = [0, 1];
   }
   get delta() {
-    return this.deltas[this.heading];
+    return this.#deltas[this.#heading];
   }
   turn(direction) {
-    this.heading = direction;
+    this.#heading = direction;
   }
   isSameOrOppositeDirection(direction) {
-    return this.heading % 2 === direction % 2;
+    return this.#heading % 2 === direction % 2;
   }
 }
